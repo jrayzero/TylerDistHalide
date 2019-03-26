5637f811c12389173cf7bb4c6391144bfa5ec5c2
Jessica Ray <jray@csail.mit.edu>
2019-03-25 23:38:49 -0400

    cleanup sobel dist

--- apps/sobel/sobel_distributed.cpp
+++ apps/sobel/sobel_distributed.cpp
@@ -7,21 +7,8 @@ using namespace Halide;
 #include "mpi_timing.h"
 
 DistributedImage<float> input, output;
-Image<float> global_input, global_output;
 
 Var x("x"), y("y");
-
-std::default_random_engine generator(0);
-std::uniform_real_distribution<float> distribution(0, 1);
-
-bool float_eq(float a, float b) {
-    const float thresh = 1e-5;
-    return a == b || (std::abs(a - b) / b) < thresh;
-}
-
-float rndflt() {
-    return distribution(generator);
-}
 
 Func build(bool distributed) {
     Func clamped;
@@ -45,7 +32,6 @@ Func build(bool distributed) {
 
     sobelx.vectorize(x, 8).compute_at(sobel, y);
     sobely.vectorize(x, 8).compute_at(sobel, y);
-    sobel.parallel(y);
 
     if (distributed) {
         sobel.distribute(y);
@@ -60,23 +46,14 @@ int main(int argc, char **argv) {
     int rank = 0, numprocs = 0;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
-
+    std::cerr << "Total ranks: " << numprocs << std::endl;
     const int w = argc > 1 ? std::stoi(argv[1]) : 1000;
     const int h = argc > 2 ? std::stoi(argv[2]) : 1000;
 
-    // Image<float> global_input = load<float>(argv[1]);
-    // Image<float> global_output(global_input.width(), global_input.height());
-
-    // const int w = global_input.width(), h = global_input.height();
-
-
     input = DistributedImage<float>(w, h);
     output = DistributedImage<float>(w, h);
-    // global_input = Image<float>(w, h);
-    // global_output = Image<float>(w, h);
 
     Func sobel_distributed = build(true);
-    // Func sobel_correct = build(false);
 
     output.set_domain(x, y);
     output.placement().distribute(y);
@@ -86,27 +63,15 @@ int main(int argc, char **argv) {
     input.placement().distribute(y);
     input.allocate(sobel_distributed, output);
 
-    // for (int y = 0; y < h; y++) {
-    //     for (int x = 0; x < w; x++) {
-    //         float v = x + y;
-    //         if (input.mine(x, y)) {
-    //             int lx = input.local(0, x), ly = input.local(1, y);
-    //             //input(lx, ly) = global_input(x, y);
-    //             input(lx, ly) = v;
-    //         }
-    //         //global_input(x, y) = v;
-    //     }
-    // }
-
     for (int y = 0; y < input.height(); y++) {
         for (int x = 0; x < input.width(); x++) {
 	  input(x, y) = (rank+x+y);
         }
     }
-
+    sobel_distributed.compile_to_lowered_stmt("blah.txt", {input});
     sobel_distributed.realize(output.get_buffer());
 
-#ifdef DUMP_RESULTS
+    /*#ifdef DUMP_RESULTS
     std::string fname = "rank_" + std::to_string(rank) + "_w" + std::to_string(w) + "_h" + std::to_string(h) + ".txt";
     std::ofstream out_file;
     out_file.open(fname);
@@ -116,14 +81,9 @@ int main(int argc, char **argv) {
       }
     }
     out_file.close();
-#endif
-
-    // sobel_correct.realize(global_output);
+    #endif*/
 
     const int niters = 50;
-#ifdef USE_MPIP
-    MPI_Pcontrol(1);
-#endif
     std::vector<std::chrono::duration<double,std::milli>> duration_vector_1;
     for (int i = 0; i < niters; i++) {
         MPI_Barrier(MPI_COMM_WORLD);
@@ -135,73 +95,11 @@ int main(int argc, char **argv) {
         duration_vector_1.push_back(duration1);
     }
 
-    // for (int y = 0; y < output.height(); y++) {
-    //     for (int x = 0; x < output.width(); x++) {
-    //         int gx = output.global(0, x), gy = output.global(1, y);
-    //         if (!float_eq(output(x, y), global_output(gx, gy))) {
-    //             printf("[rank %d] output(%d,%d) = %f instead of %f\n", rank, x, y, output(x, y), global_output(gx, gy));
-    //             MPI_Abort(MPI_COMM_WORLD, 1);
-    //             MPI_Finalize();
-    //             return -1;
-    //         }
-    //     }
-    // }
-
     if (rank == 0) {
         printf("Sobel test succeeded!\n");
 	print_time("performance_CPU.csv", "### sobel_" + std::to_string(w) + "_" + std::to_string(h), {"DistHalide"},
 		 {median(duration_vector_1)});
     }
-
-    // if (rank == 0) {
-    //     if (numprocs > 1) {
-    //         for (int r = 1; r < numprocs; r++) {
-    //             MPI_Status status;
-    //             int numrows, rowwidth, startx, starty;
-    //             MPI_Recv(&numrows, 1, MPI_INT, r, 0, MPI_COMM_WORLD, &status);
-    //             MPI_Recv(&rowwidth, 1, MPI_INT, r, 0, MPI_COMM_WORLD, &status);
-    //             MPI_Recv(&startx, 1, MPI_INT, r, 0, MPI_COMM_WORLD, &status);
-    //             MPI_Recv(&starty, 1, MPI_INT, r, 0, MPI_COMM_WORLD, &status);
-
-    //             int maxcount = output.global_width();
-    //             float *data = new float[maxcount];
-    //             for (int i = 0; i < numrows; i++) {
-    //                 MPI_Recv(data, maxcount, MPI_FLOAT, r, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    //                 int gx = startx, gy = starty + i;
-    //                 for (int i = 0; i < rowwidth; gx++, i++) {
-    //                     global_output(gx, gy) = data[i];
-    //                 }
-    //             }
-    //             delete[] data;
-    //         }
-    //     }
-
-    //     for (int y = 0; y < h; y++) {
-    //         for (int x = 0; x < w; x++) {
-    //             if (output.mine(x, y)) {
-    //                 int lx = output.local(0, x), ly = output.local(1, y);
-    //                 global_output(x, y) = output(lx, ly);
-    //             }
-    //         }
-    //     }
-    //     save(global_output, argv[2]);
-    // } else {
-    //     int numrows = output.height(), rowwidth = output.width(),
-    //         startx = output.global(0, 0), starty = output.global(1, 0);
-    //     MPI_Send(&numrows, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
-    //     MPI_Send(&rowwidth, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
-    //     MPI_Send(&startx, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
-    //     MPI_Send(&starty, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
-    //     for (int y = 0; y < output.height(); y++) {
-    //         int count = output.width();
-    //         float *data = new float[count];
-    //         for (int x = 0; x < output.width(); x++) {
-    //             data[x] = output(x, y);
-    //         }
-    //         MPI_Send(data, count, MPI_FLOAT, 0, 0, MPI_COMM_WORLD);
-    //         delete[] data;
-    //     }
-    // }
 
     MPI_Finalize();
     return 0;
